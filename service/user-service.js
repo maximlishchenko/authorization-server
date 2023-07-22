6cb44e5556648b0fcb45a7f1ea6c27a02e3be473
maximlishchenko <makslis2000@gmail.com>
2023-07-22 17:49:04 +0300

    add registration with activation link

--- service/user-service.js
+++ service/user-service.js
@@ -1,5 +1,25 @@
-class UserService {
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('../service/mail-service');
+const tokenService = require('../service/token-service');
+const UserDto = require('../dtos/user-dto');
 
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email});
+        if (candidate) {
+            throw new Error(`User with email ${email} already exists.`);
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await UserModel.create({email, password: hashPassword, activationLink });
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {...tokens, user: userDto};
+    }
 }
 
 module.exports = new UserService();
